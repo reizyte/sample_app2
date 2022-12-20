3da874192dfb7936844b2262ba714d2524f6aeab
reizyte <112692614+reizyte@users.noreply.github.com>
2022-12-20 19:08:06 +0000

    [start]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,8 +17,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
     redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def update
@@ -27,8 +30,14 @@ class ListsController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+
   root :to =>'homes#top'
-  post 'lists' => 'lists#create'
-  patch 'list/:id' => 'lists#update', as: 'update_list'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
